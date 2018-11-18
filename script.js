70dc48db8349219bf92539d189f7a8efb29af82a
Mostafa <mustafa.husseiny@gmail.com>
2018-11-18 03:54:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,25 @@ $(document).ready(function() {
 	$(".box").on("click", function() {
 		$(".list").toggle(300);
 	})
+});
+
+$(function() {
+	$(window).on("scroll", function() {
+		$("#tester").text($(document).scrollTop());
+	})
+})
+$(document).ready(function() {
+	let $body = $("html, body");
+	$(".item-1").on("click", function() {
+		$body.animate({scrollTop: 802}, 1000);
+	})
+	$(".item-nav-1").on("click", function() {
+		$body.animate({scrollTop: 802}, 1000);
+	})
+	$(".item-2").on("click", function() {
+		$body.animate({scrollTop: 1501}, 1000);
+	})
+	$(".item-nav-2").on("click", function() {
+		$body.animate({scrollTop: 1501}, 1000);
+	})
 })
